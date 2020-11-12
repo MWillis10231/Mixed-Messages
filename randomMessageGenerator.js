e2a7c528b04b839430586faae676e98d82867a25
Matrim <61281235+MWillis10231@users.noreply.github.com>
2020-11-12 11:25:11 +0100

    First run test

--- randomMessageGenerator.js
+++ randomMessageGenerator.js
@@ -8,10 +8,9 @@ const starSign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra'
 const astrologicalTerm2 = ['are aligned', 'are in transit', 'have strong synastry', 'have weak synastry', 'are cardinal', 'are fixed', 'are mutable', 'are in opposition', 'are on the cusp'];
 const weatherAhead = ['stormy', 'calm', 'snowy', 'sunny', 'rainy', 'warm', 'hot', 'cold', 'wet', 'humid', 'damp', 'dry'];
 const people = ['pirate', 'thief', 'liar', 'cheat', 'honest man', 'honest woman', 'bartender', 'teacher', 'writer', 'journalist', 'smith', 'shopkeeper', 'statistician', 'scientist', 'politician', 'warrior', 'nomad', 'scholar', 'diplomat', 'lover', 'partner']
-const verbs = ['be wary of', 'look out for', 'expect', 'be vigilant of'],
+const verbs = ['be wary of', 'look out for', 'expect', 'be vigilant of'];
 const peopleAction = ['help you', 'change your life', 'take something from you', 'help you repay a debt', 'take you on a journey', 'teach you something', 'help you learn', 'show you your mistakes', 'lie to you', 'buy something from you', 'sell something to you', 'help you forget your past', 'show you your future'];
 
 let aBody = astronomicalBodies[Math.floor(Math.random()*astronomicalBodies.length)]
-console.log(aBody);
 
-return `Hello fortune-seeker. I am the venerable Oracle of Delphi and I will tell you your future.`;
+console.log(`Hello fortune-seeker. I am the venerable Oracle of Delphi and I will tell you your future.`);
